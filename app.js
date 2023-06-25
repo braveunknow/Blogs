e4ed0ebce72115ea9cc61b8b95a0075bb44561a7
braveunknow <braveunknown123@gmail.com>
2023-06-26 00:18:10 +0530

    first commint

---
+++ app.js
@@ -0,0 +1,62 @@
+//******** all requirement *********
+require('dotenv').config();
+
+// const res = require('express/lib/response');
+const express = require('express');
+//require express layout
+const expressLayout = require('express-ejs-layouts');
+//for editing and updating blogs
+const methodOverride = require('method-override');
+//cookieparser to stay looged in
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo');
+const session = require('express-session');
+
+//require database 
+const connectDB = require('./server/config/db');
+const { isActiveRoute } = require('./server/helpers/routeHelpers');
+//express is listening at port 5000
+const app = express();
+const PORT = 5000 || process.env.PORT;
+
+
+//connecting to database
+connectDB();
+
+//few middlewares so that search works
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(methodOverride('_method'));
+
+//adding cookieparser as middleware
+app.use(cookieParser());
+//for sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
+    }),
+  //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+}));
+
+// set a public folder for all js css images etc
+app.use(express.static('public'));
+
+// setting Templating Engine
+app.use(expressLayout);
+app.set('layout','./layouts/main');  //set default layout ./layout/main is the location
+app.set('view engine', 'ejs');  //set view engine + this also requires a views folder (express default setting)
+
+app.locals.isActiveRoute = isActiveRoute;
+
+//seperating routes for different page
+app.use('/',require('./server/routes/main')); //home page
+app.use('/',require('./server/routes/admin')); //admin page
+
+
+app.listen(PORT, ()=> {
+    console.log(`App Listening on port ${PORT}`);
+});
+
---
+++ server/routes/main.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+
+//get method and a home route
+//     Home page and request responce function '' refers to home [ade]
+router.get('', async (req, res) => {
+    try {
+        const locals = {        //basic object example of passing data
+            title: "nodejs blog",
+            description: "this is a basic nodejs blog"
+        }
+        
+        let perPage = 6;
+        let page = req.query.page || 1;
+
+        const data = await Post.aggregate([{$sort: { createdAt: -1 }}])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec();
+
+        const count = await Post.count();
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+        res.render('index',{ 
+            locals, 
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage : null,
+            currentRoute: '/'
+         });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+// get Post: id
+router.get('/post/:id', async (req, res) => {
+  try {
+    let slug = req.params.id;
+    const data = await Post.findById({_id: slug });
+  
+    const locals = {
+        title: data.title,
+        description: "Simple Blog created with NodeJs, Express & MongoDb.",
+        currentRoute: `/post/&{slug}`
+    }
+    res.render('post', { locals, data });
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+
+//post method for search
+router.post('/search', async (req, res) => {
+
+
+  try {
+    const locals = {
+        title: "Search",
+        description: "Simple Blog created with NodeJs, Express & MongoDb."
+    }
+
+    let  searchTerm = req.body.searchTerm;
+    const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "")
+    const data = await Post.find({
+        $or: [
+            {  title: {$regex: new RegExp(searchNoSpecialChar, 'i')}},
+            {  body: {$regex: new RegExp(searchNoSpecialChar, 'i')}}
+        ]
+    });
+    res.render("search", {
+        data,
+        locals
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+
+router.get('/about',(req,res) => {    // this is how to add new routes
+    res.render('about');
+    currentRoute: '/about'
+});
+
+module.exports = router;
+
+
+//routes can be splitup into controlleres
